cb248d554953302e6d0b969862acba01c97db55b
magicteo <matty.macaluso@gmail.com>
2022-10-25 09:58:36 +0200

    Minor changes

--- app/src/main/java/com/example/nbafantasyassistant/MainActivity.kt
+++ app/src/main/java/com/example/nbafantasyassistant/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.nbafantasyassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.player)
+        button = findViewById(R.id.test_button)
+
     }
 }
