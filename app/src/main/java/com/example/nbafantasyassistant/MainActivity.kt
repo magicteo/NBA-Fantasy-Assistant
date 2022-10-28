cad41957f12602f13ad4a3c79a88d123cee2dbf7
magicteo <matty.macaluso@gmail.com>
2022-10-28 08:18:14 +0200

    Minor changes

--- app/src/main/java/com/example/nbafantasyassistant/MainActivity.kt
+++ app/src/main/java/com/example/nbafantasyassistant/MainActivity.kt
@@ -5,18 +5,27 @@ import android.os.Bundle
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.TextView
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var text: TextView
+
     private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text = findViewById(R.id.player)
-        button = findViewById(R.id.test_button)
+        val textView: TextView = findViewById<TextView>(R.id.modifiable)
+        button = findViewById(R.id.button)
+
+        val url = "https://www.rotowire.com/basketball/injury-report.php"
+        val doc = Jsoup.connect(url).get()
+        val events = doc.select("h1.page-title__primary")
+
+        button.setOnClickListener{
+            textView.text = events.toString()
+        }
 
     }
 }
